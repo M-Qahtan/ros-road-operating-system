b7aff3e1c5a6fcd28a06bc78ec0ab59ca9147dd8
Mohammed Qahtan <125224479+M-Qahtan@users.noreply.github.com>
2026-07-25 06:25:08 +0300

    export RoadEvent repository port

--- packages/domain/src/index.ts
+++ packages/domain/src/index.ts
@@ -1,3 +1,4 @@
 export * from './road-event/road-event.js';
+export * from './road-event/road-event-repository.js';
 export * from './road-event/road-event-status.js';
 export * from './road-event/severity.js';
