6453bdefdc007f2fbea8815d8fa89c250be228a9
Mohammed Qahtan <125224479+M-Qahtan@users.noreply.github.com>
2026-08-24 01:25:07 +0300

    security: remove runtime evidence delete authority

--- infrastructure/staging/aws/iam.tf
+++ infrastructure/staging/aws/iam.tf
@@ -72,14 +72,13 @@ data "aws_iam_policy_document" "api_evidence_access" {
   }
 
   statement {
-    sid    = "ManageEvidenceObjects"
+    sid    = "ReadWriteEvidenceObjectsWithoutDelete"
     effect = "Allow"
     actions = [
       "s3:GetObject",
       "s3:GetObjectAttributes",
       "s3:GetObjectVersion",
-      "s3:PutObject",
-      "s3:DeleteObject"
+      "s3:PutObject"
     ]
     resources = ["${aws_s3_bucket.evidence.arn}/*"]
   }
